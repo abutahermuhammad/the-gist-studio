c37b09398a0b56b882a6495bfb54571f6f651b94
Abu Taher Muhammad <abut1081@gmail.com>
2021-12-05 12:57:03 +0600

    [UPDATE] bodyContent schema

--- schemas/blockContent.js
+++ schemas/blockContent.js
@@ -57,7 +57,23 @@ export default {
     // as a block type.
     {
       type: 'image',
-      options: {hotspot: true}
+      options: {hotspot: true},
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+          options: {
+            isHighlighted: true // <-- make this field easily accessible
+          }
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        }
+      ]
     }
   ]
 }
